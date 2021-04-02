b8cb54a2c78bdafabb0080762bd92218eda41a55
Maeckeul <cmaeckeul@gmail.com>
2021-04-01 22:25:15 -1000

    :alembic: déclaration de fonction, expression de fonction, boucle for, for of et methode forEach

--- js/script.js
+++ js/script.js
@@ -4,3 +4,33 @@ var fruits = ['pomme', 'poire', 'orange'];
 console.log('je mange : ' + fruits[0]);
 
 console.log(`Je mange : ${fruits[0]}`);
+
+// boucle for
+for (var fruitIndex = 0; fruitIndex < fruits.length; fruitIndex++)
+{
+    console.log(`je mange ${fruits[fruitIndex]}`);
+}
+
+// boucle for ... of
+// debugger;
+for (let fruit of fruits)
+{
+    setTimeout(function() {
+        console.log(`Je mange ${fruit}`);
+    }, 1000);
+}
+
+// déclaration de fonction
+// function log(fruitInconnu, fruitIndex)
+// {
+//     console.log(`je mange ${fruitInconnu} ${fruitIndex}`);
+// }
+
+// expression de fonction
+let log = function(fruitInconnu, fruitIndex) 
+{
+    console.log(`je mange ${fruitInconnu} ${fruitIndex}`);
+}
+
+// methode forEach
+fruits.forEach(log);
